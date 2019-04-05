fe4662db0c34270f7963e5c57b8d1d8a87a29bef
Nimisoere <lawson.nimi@yahoo.com>
2019-04-05 17:36:26 +0100

    Updates

---
+++ precache-manifest.64a526262b435bb964a79fbec70a19d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaa63ff45b2c7ee705b2",
+    "url": "/personio-applications/static/css/main.30b0e9bd.chunk.css"
+  },
+  {
+    "revision": "eaa63ff45b2c7ee705b2",
+    "url": "/personio-applications/static/js/main.21c23b01.chunk.js"
+  },
+  {
+    "revision": "e841970b67d18c772339",
+    "url": "/personio-applications/static/js/runtime~main.f223b819.js"
+  },
+  {
+    "revision": "1e7c37005df8a4e1a2ae",
+    "url": "/personio-applications/static/css/2.cc5ce909.chunk.css"
+  },
+  {
+    "revision": "1e7c37005df8a4e1a2ae",
+    "url": "/personio-applications/static/js/2.1c8f2a83.chunk.js"
+  },
+  {
+    "revision": "e2a32e947908cd2b0f669c749517a4fb",
+    "url": "/personio-applications/static/media/personio_logo.e2a32e94.svg"
+  },
+  {
+    "revision": "291cd777318a20ff82333852650e6cc5",
+    "url": "/personio-applications/index.html"
+  }
+];
